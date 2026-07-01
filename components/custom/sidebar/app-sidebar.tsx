0f3fa9ddc07cc3df58d5df9ba96113393ed6b7f7
eielelle <eielelle@example.com>
2026-07-01 17:18:29 +0800

    initial commit

---
+++ components/custom/sidebar/app-sidebar.tsx
@@ -0,0 +1,245 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BarChart,
+  BookOpen,
+  Bot,
+  Coins,
+  Command,
+  Database,
+  Factory,
+  Frame,
+  GalleryVerticalEnd,
+  Home,
+  LineChart,
+  Map,
+  MapPin,
+  Package,
+  PieChart,
+  Settings,
+  Settings2,
+  ShoppingBag,
+  SquareTerminal,
+  Users,
+} from "lucide-react"
+
+import { NavMain } from "./nav-main"
+import { NavSecondary } from "./nav-secondary"
+import { NavUser } from "./nav-user"
+import { TeamSwitcher } from "./team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Production",
+      url: "#",
+      icon: Factory,
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Inventory",
+      url: "#",
+      icon: Package,
+      isActive: true,
+      items: [
+        {
+          title: "All Inventory",
+          url: "#",
+        },
+        {
+          title: "Returns",
+          url: "#",
+        },
+        {
+          title: "Pullouts",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Sales",
+      url: "#",
+      icon: ShoppingBag,
+      isActive: true,
+      items: [
+        {
+          title: "Sales to Trade",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Expenses",
+      url: "#",
+      icon: Coins,
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Visits",
+      url: "#",
+      icon: MapPin,
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Reports",
+      url: "#",
+      icon: BarChart,
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Database",
+      url: "#",
+      icon: Database,
+      isActive: true,
+      items: [
+        {
+          title: "Item Master Data",
+          url: "#",
+        },
+        {
+          title: "Outlets",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Administration",
+      url: "#",
+      icon: Settings,
+      isActive: true,
+      items: [
+        {
+          title: "Authorizations",
+          url: "#",
+        },
+        {
+          title: "Organizations",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "Feed",
+      url: "#",
+      icon: Home,
+    },
+    {
+      name: "Tracking",
+      url: "#",
+      icon: LineChart,
+    },
+    {
+      name: "Employees",
+      url: "#",
+      icon: Users,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavSecondary projects={data.projects} />
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ forms/auth/forgot-password.form.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export default function ForgotPassword() {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+
+      <Input />
+
+      <Input />
+
+      <Button>Request Password</Button>
+    </div>
+  )
+}
